8cc2e977b9cbca3b116f0aa3e6bd36636d8b5d2c
Gabriel <gabrielpachecotec@gmail.com>
2024-11-27 15:59:18 +0000

    Change todos-factory to todos-module

--- src/todos-factory.js
+++
@@ -1,8 +0,0 @@
-export function makeTodoItemObject(title, description, dueDate, priority) {
-    return {
-        title: title,
-        description: description,
-        dueDate: dueDate,
-        priority: priority, 
-    }
-}
