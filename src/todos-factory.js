8f23631e7281269cd27dee34832c232ae47db29e
Gabriel <gabrielpachecotec@gmail.com>
2024-11-27 11:47:18 +0000

    Add template.html, index.js and todos module
    Inside the todos module I added the makeTodoItemObject factory
    function, this function creates individual todos.

---
+++ src/todos-factory.js
@@ -0,0 +1,8 @@
+export function makeTodoItemObject(title, description, dueDate, priority) {
+    return {
+        title: title,
+        description: description,
+        dueDate: dueDate,
+        priority: priority, 
+    }
+}
