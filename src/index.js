ad69f4787aa37041b31c94623d8d5754c9141a4e
Gabriel <gabrielpachecotec@gmail.com>
2024-11-29 13:20:51 +0000

    Add homepage dynamically when page is loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "./styles.css"
+import './styles.css'
+import './styles-home.css'
+
+import logo from './todo-list-logo.png'
+
+import { generateHomePage } from "./home"
+
+function main() {
+    generateHomePage('id', 'content', logo);
+}
+
+main();
