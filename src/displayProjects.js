aafad3a1041ec31aee4033abf04f1bbd2dcff191
Gabriel <gabrielpachecotec@gmail.com>
2024-12-10 08:47:56 +0000

    Write createProjectAndSaveItToLocalStorage function

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -6,7 +6,7 @@ import { makeTodoItemObject, createTodosProject, Project, Projects } from './tod
 export function generateProjectsPage(selectorType, selectorName) {
     const homeContent = selectDomElement(selectorType, selectorName);
     removeElementContent(homeContent);
-    
+
     const heading = generateProjectsHeading()
     homeContent.appendChild(heading);
 }
@@ -16,4 +16,10 @@ function generateProjectsHeading() {
     heading1.className = 'heading-projects';
     heading1.innerText = 'Projects';
     return heading1;
+}
+
+function createProjectAndSaveItToLocalStorage(projectName) {
+    const project = createTodosProject(projectName);
+    Projects.addProject(project.name, defaultProject.itemsArray);
+    Projects.updateLocalStorage();
 }
