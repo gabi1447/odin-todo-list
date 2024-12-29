20a70e076e2a37df3a7e0c93eb828eeea06661b9
Gabriel <gabrielpachecotec@gmail.com>
2024-12-29 09:44:06 +0000

    Add project to local storage from modal and update projects page

--- src/projects-modal.js
+++ src/projects-modal.js
@@ -1,13 +1,39 @@
+import { createTodosProject, Projects } from "./todos-module";
+import { generateProjectsPage } from "./displayProjects";
+
 export function setupModalEventListeners() {
     const modal = document.querySelector('#projects-modal');
     const openModal = document.querySelector('.add-new-project');
     const closeModal = document.querySelector('.modal-close-btn');
+    const projectModalForm = document.querySelector('.add-project-modal-form');
 
     openModal.addEventListener('click', () => {
         modal.showModal();
     })
 
     closeModal.addEventListener('click', () => {
+        modal.close();
+    })
+
+    projectModalForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const projectName = document.querySelector('#project-name');
+
+        if (projectName.value) {
+            // ADD PROJECT TO GENERAL PROJECTS
+            const newProject = createTodosProject(projectName.value);
+            Projects.addProject(newProject.name, newProject.itemsArray);
+            Projects.updateLocalStorage();
+
+            // TESTING
+            console.log(projectName.value);
+
+            // REMOVE CONTENT FROM INPUT
+            projectName.value = '';
+        }
+
+        // UPDATE PROJECTS PAGE AND CLOSE MODAL
+        generateProjectsPage('id', 'content');
         modal.close();
     })
 }
