4bd65629dfa6f4137a797c9dab0dd6c95e4db1ba
Gabriel <gabrielpachecotec@gmail.com>
2024-12-30 10:19:47 +0000

    Change name of selectors object

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -3,16 +3,16 @@ import { generateProjectsPage } from "./displayProjects";
 
 import logo from "./todo-list-logo.png";
 
-const targetStaticElements = {
+const selectors = {
     home: document.querySelector('#home'),
     projects: document.querySelector('#projects')
 }
 
 export function setupStaticEventListeners() {
-    targetStaticElements.home.addEventListener('click', () => {
+    selectors.home.addEventListener('click', () => {
         generateHomePage('id', 'content', logo, 'Plan and Organize');
     });
-    targetStaticElements.projects.addEventListener('click', () => {
+    selectors.projects.addEventListener('click', () => {
         generateProjectsPage('id', 'content');
     });
 }
