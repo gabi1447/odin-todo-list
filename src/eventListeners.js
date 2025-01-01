a5e1961ca06561cfa498c399a1284b840836536b
Gabriel <gabrielpachecotec@gmail.com>
2025-01-01 13:55:46 +0000

    Refactor

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -3,15 +3,16 @@ import { generateProjectsPage } from "./displayProjects";
 
 import logo from "./todo-list-logo.png";
 
-const selectors = {
-    home: document.querySelector('#home'),
-    projects: document.querySelector('#projects')
-}
-
 export function setupStaticEventListeners() {
+    const selectors = {
+        home: document.querySelector('#home'),
+        projects: document.querySelector('#projects')
+    }
+
     selectors.home.addEventListener('click', () => {
         generateHomePage('id', 'content', logo, 'Plan and Organize');
     });
+
     selectors.projects.addEventListener('click', () => {
         generateProjectsPage('id', 'content');
     });
