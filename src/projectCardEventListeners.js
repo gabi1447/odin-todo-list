ec3cb4a6617bed145830039012f1105a4282d4b9
Gabriel <gabrielpachecotec@gmail.com>
2025-01-07 09:12:54 +0000

    Refactor

--- src/projectCardEventListeners.js
+++ src/projectCardEventListeners.js
@@ -4,19 +4,19 @@ import { generateIndividualProjectPage } from "./displayIndividualProject";
 
 export function setupProjectCardButtonsEventListeners() {
     const mainContent = document.querySelector('#content');
+    
     mainContent.addEventListener('click', (event) => {
         if (event.target.classList.contains('delete-button')) {
             const projectToBeRemoved = event.target.parentElement.parentElement.id;
             Projects.removeProject(projectToBeRemoved);
             Projects.updateLocalStorage();
-            
-            // Refresh projects page
+
             generateProjectsPage('id', 'content');
+
         } else if (event.target.classList.contains('visit-button')) {
             const projectToVisitName = event.target.parentElement.parentElement.id;
-            const arrayOfTodosOfVisitedProject = Projects.retrieveProjectsDataFromLocalStorage()[projectToVisitName];
 
-            generateIndividualProjectPage(projectToVisitName, arrayOfTodosOfVisitedProject);
+            generateIndividualProjectPage(projectToVisitName);
         }
     });
 }
