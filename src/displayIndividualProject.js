31cb07edfece5997b08d88170da1c253be40bf6c
Gabriel <gabrielpachecotec@gmail.com>
2025-01-07 09:23:34 +0000

    Add render function and event listeners of individual project modal

--- src/displayIndividualProject.js
+++ src/displayIndividualProject.js
@@ -1,16 +1,11 @@
 import { generateButton } from "./displayProjects";
 import { addClassToSelector, removeElementContent, selectDomElement } from "./generalPurposeModule";
+import { Projects } from "./todos-module";
+import { setupIndividualProjectModalEventListeners } from "./addTodoItemToProjectModal";
 
 const CLASSNAME = 'individual-project-content';
 
-export function generateIndividualProjectPage(projectName, arrayOfTodosProject) {
-    // Takes project object builds individual project page using
-    // its name field and array of items for the heading and list
-    // of todos
-
-    // Testing
-    console.log(projectName);
-    console.log(arrayOfTodosProject);
+export function generateIndividualProjectPage(projectName) {
 
     const mainContent = selectDomElement('id', 'content');
     // clear content of div content
@@ -23,6 +18,8 @@ export function generateIndividualProjectPage(projectName, arrayOfTodosProject) 
     const addTodoItemBtn = generateButton('add-todo-item-btn', '', 'Add Todo');
     mainContent.appendChild(addTodoItemBtn);
     // render todo items of project if there are any in project's array of todos
+    renderTodoItems(projectName);
+    setupIndividualProjectModalEventListeners();
 }
 
 function generateIndividualProjectHeading(projectName) {
@@ -31,4 +28,12 @@ function generateIndividualProjectHeading(projectName) {
     projectHeading.className = 'individual-project-heading';
 
     return projectHeading;
+}
+
+function renderTodoItems(projectName) {
+    const projectsObject = Projects.retrieveProjectsDataFromLocalStorage();
+    const arrayOfItemsOfIndividualProject = projectsObject[projectName];
+    arrayOfItemsOfIndividualProject.forEach(todo => {
+        console.log(todo);
+    })
 }
